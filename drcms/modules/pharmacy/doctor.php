36f7b3b2e493e00639a5988536a506492b3f5a38
WWw178430166 <www178430166@qq.com>
2021-01-05 15:34:21 +0800

    医生端处方模板

--- drcms/modules/pharmacy/doctor.php
+++ drcms/modules/pharmacy/doctor.php
@@ -62,22 +62,154 @@ class doctor extends authority{
 		$data = explode('|',trim($_POST['data'],'|'));
 		$title = $_POST['title'];
         $pnname = $_POST['pnname'];
-		$this->default_db->load('drug_pn');
-		if($ac == 'edit'){
+		/*if($ac == 'edit'){
 			$eids = explode(',',trim($_POST['eids'],','));
 			$da = $this->default_db->delete('`id` in ('.trim($_POST['eids'],',').')');
+		}*/
+		if ($ids) {
+		    $pnid = date('YmdHis');//会出现重复
+		    //药品信息
+		    $this->default_db->load('drug');
+		    $_drug = $this->default_db->select(['id'=>['in'=>$ids]],'id,trade_name,specification,enterprise,vip_price');
+		    foreach ($_drug as $v) {
+		        $drug[$v['id']] = $v;
+		    }
+		    //生成处方单
+		    $user_id = 0;//提交
+		    $time = time();
+		    $this->default_db->load('drug_pn');
+    		foreach($ids as $k=>$v){
+    		    /*var_dump(array('userid'=>$user_id,'data_str'=>$data[$k],'pnname'=>$pnname,'title'=>$title,'dname'=>$drug[$v]['trade_name'],'add_time'=>$time,'pnid'=>$pnid,'rid'=>$v));
+    		    die;*/
+    			if($v){
+    			    //,'data1'=>$tmpDa[1],'data2'=>$tmpDa[2],'data3'=>$tmpDa[3],'data4'=>$tmpDa[4],'data5'=>$tmpDa[5],'data6'=>$tmpDa[6],
+    				$this->default_db->insert(array('userid'=>$user_id,'data_str'=>$data[$k],'pnname'=>$pnname,'title'=>$title,'dname'=>$drug[$v]['trade_name'],'add_time'=>$time,'pnid'=>$pnid,'rid'=>$v));
+    			}
+    		}
+    		//var_dump($this->default_db->error());
+    		//创建正式处方单
+    		//$this->default_db->load('prescription');
 		}
-		$pnid = date('YmdHis');
-        $tmpDa2 = explode(',',$_POST['dname']);
-		foreach($ids as $k=>$v){
-			if($v){
-				$tmpDa = explode(',',$data[$k]);
-                //echo '<pre>';var_dump($tmpDa);echo '</pre>';
-				$this->default_db->insert(array('userid'=>$this->userid,'data_str'=>$data[$k],'pnname'=>$pnname,'title'=>$title,'dname'=>$tmpDa2[$k],'data1'=>$tmpDa[1],'data2'=>$tmpDa[2],'data3'=>$tmpDa[3],'data4'=>$tmpDa[4],'data5'=>$tmpDa[5],'data6'=>$tmpDa[6],'add_time'=>time(),'pnid'=>$pnid,'rid'=>$v));
-			}
-		}
+		
         exit('{"status":"1","pnid":"'.$pnid.'","title":"'.$title.'"}');
 	}
+
+	public function getAllPN(){   
+		$where = '`display` = 0';
+		if($_REQUEST['search'] && $_REQUEST['search'] != 'undefined'){
+			$where .= ' AND `dname` like "%'.$_REQUEST['search'].'%"';
+		}
+		$this->default_db->load('drug_pn');
+		$da = $this->default_db->select($where,'*','','add_time DESC');
+		
+		foreach($da as $v) $data[$v['pnid']][] = $v;
+		exit(json_encode($data));
+	}
+
+	public function docGetDrug(){
+        $user_id = intval($_POST['user_id']);
+        $where = '`user_id` = '.$user_id.' AND ';
+        if(!$user_id) $where = '';
+	    $this->default_db->load('member_prescription');
+	    $da = $this->default_db->listinfo($where.'`doctor_user_id` = '.$this->user_id,'create_time DESC',$this->page,20);
+
+	    foreach($da as $v){
+            $pid[$v['prescription_no']] = $v['prescription_no'];
+            $pnDid[$v['id']] = $v['id'];
+            $pnddd[$v['id']] = $v['prescription_no'];
+        }
+        $this->default_db->load('member_prescription_data');
+        $pnDataD = $this->default_db->select(array('prescription_id'=>array('in',$pnDid)));
+	    foreach($pnDataD as $v) $pnMsg[$v['prescription_no']][$v['drug_id']] = $v['data_str'];
+
+        $this->default_db->load('order3_prescription');
+        $pidArr = $this->default_db->select(array('prescription_no'=>array('in',$pid)));
+        foreach($pidArr as $v){
+            $oid[$v['order_no']] = $v['order_no'];
+            $pnDataDa[$v['order_no']] = $v;
+        }
+        $this->default_db->load('order3');
+        if($_POST['sdate'] && $_POST['edate']) {
+            $whereArr = array('order_no' => array('in', $oid), 'create_time' => array('>', strtotime($_POST['sdate'].' 00:00:00')), 'create_time' => array('<', strtotime($_POST['edate'].' 23:59:59')));
+            $whereStr = '`order_no` in ('.implode(',',$oid).') AND `create_time` > '.strtotime($_POST['sdate'].' 00:00:00').' AND `create_time` < '.strtotime($_POST['edate'].' 23:59:59');
+            if($_GET['status'] >= 1){
+                $whereArr['status'] = intval($_GET['status']);
+                $whereStr .= ' AND `status` = '.intval($_GET['status']);
+            }
+            $datas = $this->default_db->select($whereStr,'*','','create_time DESC');
+            if($_GET['status'] > 0) $payTxt = '`status` = '.intval($_GET['status']);else $payTxt = '';
+            $payMoney = $this->default_db->select($payTxt,'id,status,pay_money');
+        }else{
+            $whereArr = array('order_no'=>array('in',$oid));
+            if($_GET['status'] >= 1) $whereArr['status'] = intval($_GET['status']);
+            $datas = $this->default_db->select($whereArr,'*','','create_time DESC');
+            if($_GET['status'] > 0) $payTxt = '`status` = '.intval($_GET['status']);else $payTxt = '';
+            $payMoney = $this->default_db->select($payTxt,'id,status,pay_money');
+        }
+        $payMoneyNum = 0;
+        foreach($payMoney as $v) $payMoneyNum += $v['pay_money'];
+        if ($datas) {
+            $status = 1;
+            $order_ids = array();
+            $statuss = array('已取消','待付款','已付款','已发货','交易成功');
+            foreach($datas as $k=>$r){
+                $order_ids[] = $r['id'];
+                $datas[$k]['order_no_'] = $r['order_no'];
+                $datas[$k]['order_no'] = substr_cut($r['order_no'],5,5);
+                $datas[$k]['user_name'] = substr_cut($r['user_name'],3,4);
+                $datas[$k]['create_time'] = date('Y-m-d H:i:s',$r['create_time']);
+                $datas[$k]['statusStr'] = $statuss[$r['status']];
+            }
+            if ($order_ids) {
+                $where2 = array('order_id'=>array('in',$order_ids));
+                $this->default_db->load('order3_data');
+                $_product = $this->default_db->select($where2);
+                foreach($_product as $r){
+                    $r['product_img'] = '/drug/images/yaopin.jpg';
+                    $product[$r['order_id']][] = $r;
+                }
+            }
+            $merchant = array(1=>array('id'=>1,'name'=>'乐享健官方店'));
+        } else {
+            $status = 0;
+            $datas = $product = $merchant = array();
+        }
+        if(!$_GET['status'] || $_GET['status'] == 1){
+            $where = '';
+            $this->default_db->load('member_prescription');
+            $newDa = $this->default_db->listinfo($where.'`doctor_user_id` = '.$this->user_id.' AND `process` = 1','create_time DESC',$this->page,20);
+            foreach($newDa as $v){
+                $pid[$v['prescription_no']] = $v['prescription_no'];
+                $pnDid2[$v['id']] = $v['id'];
+                $userId[$v['user_id']] =$v['user_id'];
+                $pnDid_[$v['id']] = $v['user_id'];
+            }
+            $this->default_db->load('member_prescription_data');
+            $pnDataD = $this->default_db->select(array('prescription_id'=>array('in',$pnDid2)));
+            foreach($pnDataD as $v){
+                $v['user_id'] = $pnDid_[$v['prescription_id']];
+                $pnDataD_[$v['prescription_no']][] = $v;
+                $pnMsg[$v['prescription_no']][$v['drug_id']] = $v['data_str'];
+                $memberDa2_[$v['prescription_no']] = $v['user_id'];
+                $tmpData = explode('_',$v['data_str']);
+                $payMoneyNum += $tmpData[6] * $v['drug_price'];
+            }
+            $this->default_db->load('member');
+            $memberDa = $this->default_db->select(array('userid'=>array('in',$userId)),'userid,username,nickname');
+            foreach($memberDa as $v){
+                $v['username'] = substr_cut($v['username'],3,4);
+                $memberDa_[$v['userid']] = $v;
+            }
+            if($memberDa) $status = 1;
+        }
+        $jsonData = array(
+            'status'=>$status,
+            'data'=>array('pnddd'=>$pnddd,'memberDa2_'=>$memberDa2_,'pnDataD'=>$pnDataD_,'memberDa'=>$memberDa_,'datas'=>$datas,'product'=>$product,'merchant'=>$merchant,'pnMsg'=>$pnMsg,'pnDataDa'=>$pnDataDa),
+            'payMoneyNum'=>$payMoneyNum
+        );
+        exit(json_encode($jsonData));
+	}
+
 
 	public function doDefault(){
 		if ($this->ajax) {
